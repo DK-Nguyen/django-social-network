5669441fbe92c0ebeacc7c46301d9cbeb583ca51
Khoa Nguyen <nguyen29@student.tut.fi>
2018-11-01 10:49:59 +0200

    added some comments about urls as asked

--- bwa2018djangoproject/urls.py
+++ bwa2018djangoproject/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('users.urls')),
 ]
 
+# if setting is DEBUG, then use the media folder to keep the static files (images, JavaScript, or CSS)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -10,3 +10,4 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', users_views.profile, name='profile'),
 ]
+
