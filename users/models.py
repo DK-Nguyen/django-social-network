9d3f2f00025b879620424bea01cc7c1e7906bcb3
Luong Dang Hai <hailuongdang1998@gmail.com>
2018-11-01 09:13:34 +0200

    make default value correctly for email_verified

--- users/models.py
+++ users/models.py
@@ -8,14 +8,10 @@ class SiteUser(AbstractUser):
     first_name = models.CharField(max_length=30, blank=False)
     last_name = models.CharField(max_length=150, blank=False)
     email = models.EmailField(blank=False)
-    email_verified = models.BooleanField(null=False)
+    email_verified = models.BooleanField(null=False, default=False)
     phone_number = models.CharField(max_length=15, blank=False)
     address = models.TextField(blank=False)
     bio = models.TextField(blank=True)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.email_verified = False
 
     def name(self):
         return self.first_name + self.last_name
