ad7c3590ac14ba26e60774b28b7acb8782c7ac93
Joni Seppälä <joni.seppala@student.tut.fi>
2018-11-03 15:52:04 +0200

    Fixing models.py and some new fields

--- discussion/models.py
+++ discussion/models.py
@@ -1,15 +1,21 @@
 from django.db import models
+from django.conf import settings
+
 
 class Discussion(models.Model):
-    id = models.PositiveSmallIntegerField(unique = True, blank = False)
-    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length = 150, blank = False)
-    description = models.TextField(blank = True)
-    created_date = models.DateTimeField(blank = False)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150, blank=False)
+    description = models.TextField(blank=True)
+    created_date = models.DateTimeField(blank=False)
+
 
 class DiscussionComment(models.Model):
-    id = models.PositiveSmallIntegerField(unique = True, blank = False)
-    discussion_id = models.ForeignKey(Discussion, on_delete=models.CASCADE)
-    commenter_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = TextField(blank = False)
-    created_date = models.DateTimeField(blank = False)
+    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
+    commenter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(blank=False)
+    created_date = models.DateTimeField(blank=False)
+
+
+class DiscussionParticipant(models.Model):
+    id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
