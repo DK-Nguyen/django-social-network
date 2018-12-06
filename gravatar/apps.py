630d45149af94c305a811e0fefe114f64ce4df02
Luong Dang Hai <dang.luong@student.tut.fi>
2018-12-06 13:28:51 +0200

    documenting gravatar app

--- gravatar/apps.py
+++ gravatar/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class GravatarConfig(AppConfig):
+    """
+    Gravatar app is a small app to store our custom template tag that turn user's email
+    into profile picture using gravatar service
+    """
     name = 'gravatar'
--- gravatar/tags.py
+++ gravatar/tags.py
@@ -4,10 +4,15 @@ from django import template
 
 register = template.Library()
 
-# return only the URL of the gravatar
-# TEMPLATE USE:  {{ email|gravatar_url:150 }}
 @register.filter
 def gravatar_url(email, size=100):
+    """
+    This is a tag to be used in templates so that email can be used to request a profile picture
+    TEMPLATE USE:  {{ email|gravatar_url:150 }}
+    :param email: email to query
+    :param size: size of the image we want to query, by default it is 100px
+    :return: only the URL of the gravatar (string)
+    """
     default = "identicon"
     return "https://www.gravatar.com/avatar/%s?%s" % (
     hashlib.md5(str(email.lower()).encode('utf-8')).hexdigest(), urllib.parse.urlencode({'d': default, 's': str(size)}))
